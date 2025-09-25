2ae7e2d7248d50d6379d7430a26b4b6624b1e033
PRASANAMBIGAI SIVAKUMAR <prasana27ambigai@gmail.com>
2025-09-25 04:12:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6656964035-a3562",
+  "appId": "1:56753450881:web:13d2bf3c1f9ab0fd81c280",
+  "apiKey": "AIzaSyAxhQXuH2L5tamMCDKDaS1mEAEYaTu9h6s",
+  "authDomain": "studio-6656964035-a3562.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "56753450881"
+};
